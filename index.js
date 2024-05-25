121249167eb84e628ea1e444d1f560d91ba9756c
jerichodelacruz <jerichodelacruz432@gmail.com>
2024-05-25 10:19:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const count1 = document.getElementById("count1")
 function countLike1() {
   let likeCounts = parseInt(count1.value) + 1
   count1.textContent = likeCounts.toString()
-  function countLike1() {
-  let likeCounts = parseInt(count2.value) + 1
-  count2.textContent = likeCounts.toString()
 }
 btnLike1.addEventListener("click", countLike1)
+
+const btnLike1 = document.getElementById("btnLike1")
+const count1 = document.getElementById("count1")
+
+function countLike1() {
+  let likeCounts = parseInt(count1.value) + 1
+  count2.textContent = likeCounts.toString()
+}
+btnLike2.addEventListener("click", countLike1)
 
 
